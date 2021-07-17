0035b0419405668b184fa5eb833e2314bde68f84
32521amoy <67864718+32521amoy@users.noreply.github.com>
2021-07-17 15:24:57 +0800

    Update background.js
    lang=zh

--- src/bg/background.js
+++ src/bg/background.js
@@ -3,17 +3,7 @@
 //		 "sample_setting": "This is how you use Store.js to remember values"
 // });
 //example of using a message handler from the inject scripts
-var lang;
-chrome.storage.sync.get("language", function(obj) {
-    if (!obj.language) {
-        lang = chrome.i18n.getMessage("iso");
-    }
-});
-chrome.storage.onChanged.addListener(function(changes, namespace) {
-    chrome.storage.sync.get("language", function(obj) {
-        lang = obj.language;
-    });
-});
+var lang = zh;
 
 chrome.extension.onMessage.addListener(
     function(request, sender, sendResponse) {
