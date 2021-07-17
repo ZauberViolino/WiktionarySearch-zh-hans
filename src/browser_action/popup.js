67fceadfbc2799f7d7b26ee2b8355349ba3d07ce
32521amoy <67864718+32521amoy@users.noreply.github.com>
2021-07-17 12:00:30 +0800

    Update popup.js
    sinicize

--- src/browser_action/popup.js
+++ src/browser_action/popup.js
@@ -1,12 +1,3 @@
-var lang;
-chrome.storage.sync.get("language", function(obj) {
-    if (!obj.language) {
-        lang = chrome.i18n.getMessage("iso");
-    } else {
-        lang = obj.language;
-    }
-});
-localStorage.locale_pref || lang;
 window.addEventListener('load', function(evt) {
     document.getElementById('search').addEventListener('submit', openURL);
     document.getElementById("word").placeholder = chrome.i18n.getMessage("popup");
@@ -18,6 +9,6 @@ function openURL() {
     event.preventDefault();
     var word = encodeURIComponent(document.getElementById('word').value);
     chrome.tabs.create({
-        'url': "http://" + lang + ".wiktionary.org/wiki/" + word
+        'url': "https://zh.wiktionary.org/zh-hans/" + word
     });
 }
