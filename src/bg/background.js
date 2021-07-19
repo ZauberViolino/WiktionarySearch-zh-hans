1da73f17e337372839d3a344e16d1e338abe5c4f
ZauberViolino <67864718+ZauberViolino@users.noreply.github.com>
2021-07-19 16:21:45 +0800

    Update background.js

--- src/bg/background.js
+++ src/bg/background.js
@@ -5,14 +5,6 @@
 //example of using a message handler from the inject scripts
 var lang = "zh";
 
-var lang2 = "none";
-
-chrome.storage.onChanged.addListener(function(changes, namespace) {
-    chrome.storage.sync.get("language", function(obj) {
-        lang2 = obj.language;
-    });
-});
-
 chrome.extension.onMessage.addListener(
     function(request, sender, sendResponse) {
         chrome.pageAction.show(sender.tab.id);
@@ -38,32 +30,4 @@ chrome.contextMenus.create({
 
     }
 });
-
-if (lang2 !== "none") {
-    chrome.extension.onMessage.addListener(
-        function(request, sender, sendResponse) {
-           chrome.pageAction.show(sender.tab.id);
-           sendResponse();
-       });
-
-        chrome.contextMenus.create({
-       "title": chrome.i18n.getMessage("context"),
-       "contexts": ["selection"],
-       "onclick": function(e) {
-
-            var bkg = chrome.extension.getBackgroundPage();
-
-            var url = e.pageUrl;
-            var wikURL = "https://" + lang2 + ".wiktionary.org/wiki/";
-
-            if (e.selectionText) {
-                wikURL += encodeURI(e.selectionText);
-            }
-            chrome.tabs.create({
-                "url": wikURL
-            });
-
-        }
-    });    
-};
 
